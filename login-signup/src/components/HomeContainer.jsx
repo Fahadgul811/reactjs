caf3657f6b394889e58ae9549d46ac7896003d33
fahadgul811 <fahadgul811@gmail.com>
2023-07-07 19:27:15 +0500

    home container added

--- login-signup/src/components/HomeContainer.jsx
+++ login-signup/src/components/HomeContainer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./HomeContainer.module.css";
 
 const HomeContainer = () => {
-  return <div>hellow world</div>;
+  return (
+  <div>
+   
+ 
+    <div>
+      <button  className={styles.float}>+</button>
+    </div>
+   
+    
+  </div>
+ 
+    )
 };
 
 export default HomeContainer;
